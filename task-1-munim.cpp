87a250fdc5f285de6f403d28504bd9bc0a756ea4
Muskan yadav <muskanyadav92163@gmail.com>
2025-06-18 08:33:32 +0530

    complete program

--- task-1-munim.cpp
+++ task-1-munim.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
+    
+    int array[] = {2000,500,200,100,50,20,10,5,2,1};
+    int money,i,tamp;
 
+    cout << "Enter the money :- ";
+    cin >> money;
+
+    for(i=0;i<10;i++){
+        if(array[i] <= money){
+            tamp = money/array[i];
+            money = money%array[i];
+            cout << array[i] << " X " << tamp << endl;
+        }
+    }
     return 0;
 }
