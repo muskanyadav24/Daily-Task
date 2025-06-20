3d4ce21bbd72f1ef280efa0818fbcf409ba02ea2
Muskan yadav <muskanyadav92163@gmail.com>
2025-06-20 08:45:39 +0530

    Task-5-complete

--- task-5-largestDigit-oddEven-udf.cpp
+++ task-5-largestDigit-oddEven-udf.cpp
@@ -1,1 +1,37 @@
+#include<iostream>
+using namespace std;
 
+int large(int num){
+    int largest=0;
+
+    while(num > 0){
+      int digit = num % 10;
+      if(digit > largest){
+        largest = digit;
+      }
+      num/=10;
+    }
+  return largest;
+}
+
+int main(){
+
+  int num;
+
+  cout << "Enter the positive integer number :- ";
+  cin >> num;
+
+  if(num <= 0){
+    cout << "Invalid input! Please enter a positive integer. " << endl;
+  }
+  int largestDigit = large(num);
+  cout << "The largest digit in the number is: " << largestDigit << endl;
+
+  if(num%2 == 0){
+    cout << "The largest digit is even." << endl;
+  }else{
+    cout << "The largest digit is odd." << endl;
+  }
+  
+  return 0;
+}
