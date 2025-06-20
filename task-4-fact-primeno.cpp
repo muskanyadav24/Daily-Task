3dcbfb47fe16e864d806fffde65549833776c6d3
Muskan yadav <muskanyadav92163@gmail.com>
2025-06-20 08:22:00 +0530

    Task-4-complete

--- task-4-fact-primeno.cpp
+++ task-4-fact-primeno.cpp
@@ -1,1 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
 
+  int num,fact=1,count=0;
+
+  cout << "Enter the positive integer number :- ";
+  cin >> num;
+
+  cout << "Factor number is " << num << " = ";
+
+  for(int i=1;i<=num;i++){
+    if(num%i == 0){
+      cout << i << " ";
+      count++;
+    }
+  }
+  cout << endl << "Total number of factors :- " << count << endl;
+  if(count == 2){
+    cout << num << " Number is prime." ;
+  }else{
+    cout << num << " Number is not prime.";
+  }
+
+  
+  return 0;
+}
