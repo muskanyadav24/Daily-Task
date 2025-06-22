e56629a293977d225cc42e906b7265f7016ec35c
Muskan yadav <muskanyadav92163@gmail.com>
2025-06-22 13:14:38 +0530

    Task-7-complete

--- task-7-zeroes-count.cpp
+++ task-7-zeroes-count.cpp
@@ -1,1 +1,30 @@
+//Task a number as input and count how many zeroes are present using a UDF.
 
+#include<iostream>
+using namespace std;
+
+int countZeroes(int num){
+  int count = 0;
+
+  while(num > 0){
+    if(num % 10 ==0){
+      count++;
+    }
+    num/=10;
+  }
+  return count;
+}
+
+int main(){
+
+  int num;
+
+  cout << "Enter a number :- ";
+  cin >> num;
+
+  int ans = countZeroes(num);
+
+  cout << "Number of zeroes :- " << ans << endl;
+  
+  return 0;
+}
