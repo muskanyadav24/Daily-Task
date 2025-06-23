435d086d59f58c6caa9bd7a786bcae20c159ff8e
Muskan yadav <muskanyadav92163@gmail.com>
2025-06-23 21:07:09 +0530

    Task-12-Complete

--- task-12-k^th.cpp
+++ task-12-k^th.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
 
+  int a,b,c=1,d,k;
+
+  cout << "Enter the value of a :- ";
+  cin >> a;
+  cout << "Enter the value of b :- ";
+  cin >> b;
+  cout << "Enter the value of k :- ";
+  cin >> k;
+
+  for(int i=1;i<=b;i++){
+    c = c*a;
+  }
+  for(int j=1;j<=k;j++){
+    d = c%10;
+    c = c/10;
+  }
+  cout << k << "th digit from right is " << d << ".";
+  
+  return 0;
+}
+
+// Output :-
+
+// Enter the value of a :- 3
+// Enter the value of b :- 3
+// Enter the value of k :- 1
+// 1th digit from right is 7.
