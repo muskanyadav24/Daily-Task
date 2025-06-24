8ebf3de7ce8192326e33da28cef9c40dc39933e8
Muskan yadav <muskanyadav92163@gmail.com>
2025-06-24 20:25:08 +0530

    Task-13-complete

--- task-13-arr-subarray.cpp
+++ task-13-arr-subarray.cpp
@@ -1,1 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
 
+  int size,target,array[100];
+
+  cout << "Enter the size of array :- ";
+  cin >> size;
+
+  cout << "Enter the array element :- " << endl;
+
+  for(int i=0;i<size;i++){
+    cout << "Array of [" << i << "] :- ";
+    cin >> array[i];
+  }
+
+  cout << "ENter the target sum :- ";
+  cin >> target;
+
+  for(int i=0;i<size;i++){
+    for(int j=0;j<size;j++){
+      if(array[i] + array[j] == target ){
+        cout << "Indices with target sum :- [" << i << "][" << j << "]" << endl;
+        return 0;
+      }
+    }
+  }
+
+  cout << "No pair found with the target sum." << endl;
+  
+  return 0;
+  
+}
