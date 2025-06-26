daa0526dd111c727b88bf7d0af0083fd2c10958b
Muskan yadav <muskanyadav92163@gmail.com>
2025-06-26 20:31:03 +0530

    Update task-14-add-primeNo.cpp

--- task-14-add-primeNo.cpp
+++ task-14-add-primeNo.cpp
@@ -1,1 +1,40 @@
+#include<iostream>
+using namespace std;
+int main(){
 
+  int size,sum=0;
+
+  cout << "Enter the size of array :- ";
+  cin >> size;
+
+  int array[100] ;
+
+  cout << "Enter the array : " << endl;
+
+  for(int i=0;i<size;i++){
+    cout << "Array of [" << i << "] :- ";
+    cin >> array[i];
+  }
+  cout << endl << "Indices of prime numbers :- " ;//index value 
+
+  for(int i=0;i<size;i++){
+    int num = array[i];
+    int count = 0;
+
+    if(num > 1){
+      for(int j=1;j<=num;j++){
+        if(num%j == 0){
+          count++;
+        }
+      }
+      if(count == 2){
+        cout << i << " ";
+        sum += num;
+      }
+    }
+  }
+
+  cout << endl << "Sum of prime number :- " << sum << endl;
+  
+  return 0;
+}
