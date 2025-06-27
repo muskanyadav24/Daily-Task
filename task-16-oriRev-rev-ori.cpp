92515b65762920c7ab546ad55a5495c7d47977de
Muskan yadav <muskanyadav92163@gmail.com>
2025-06-27 07:01:21 +0530

    Task-16-complete

--- task-16-oriRev-rev-ori.cpp
+++ task-16-oriRev-rev-ori.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
 
+  int num,digit,original,rev=0;
+
+  cout << "Enter the positive number :- " ;
+  cin >> num;
+
+  original = num;
+  
+  while(num > 0){
+    digit = num % 10;
+    rev = rev * 10 + digit;
+    num /= 10;
+  }
+
+  cout << "Input :- " << original << endl;
+  cout << "Reversed :- " << rev << endl;
+  cout << "Difference :- " << rev-original << endl;
+  
+  return 0;
+}
