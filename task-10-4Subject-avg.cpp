99b2be2edfdd4521e9e24b4ce947bd06977c490f
Muskan yadav <muskanyadav92163@gmail.com>
2025-06-28 08:09:58 +0530

    Task-10-complete

--- task-10-4Subject-avg.cpp
+++ task-10-4Subject-avg.cpp
@@ -44,3 +44,39 @@ int main(){
   
   return 0;
 }
+
+
+// // OUTPUT :-
+
+// Enter details for 5 students :- 
+
+// Student 1 Name :- Muku
+// Math marks :- 89
+// Physics marks :- 90
+// English marks :- 99
+// Computer marks :- 90
+// Congratulations! you are a perfect student. 
+// Student 2 Name :- Kru
+// Math marks :- 90
+// Physics marks :- 78
+// English marks :- 89
+// Computer marks :- 68
+// Congratulations! you are a perfect student. 
+// Student 3 Name :- Divu
+// Math marks :- 98
+// Physics marks :- 90
+// English marks :- 89
+// Computer marks :- 67
+// Congratulations! you are a perfect student. 
+// Student 4 Name :- Rahul
+// Math marks :- 79
+// Physics marks :- 90
+// English marks :- 59
+// Computer marks :- 69
+// You are not eligible. All marks must be between 60 and 100.
+// Student 5 Name :- Angel
+// Math marks :- 67
+// Physics marks :- 79
+// English marks :- 61
+// Computer marks :- 60
+// You scored 60+ in all, but average is less than 75.
