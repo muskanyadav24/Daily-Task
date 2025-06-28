a53c37ebc7863682ae8d7b136cb6f0d18e9f0908
Muskan yadav <muskanyadav92163@gmail.com>
2025-06-28 08:18:57 +0530

    Task-18-complete

--- task-18-what-output.cpp
+++ task-18-what-output.cpp
@@ -1,1 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
 
+  int counter,first,last,next;
+  first = 1; last = 2;
+
+  for(counter=first;counter<=last;counter++){
+    cout << endl << counter;
+    next = counter*counter;
+    cout << " " << next;
+  }
+
+  counter = first;
+
+  while(counter <= last){
+    cout << endl << counter;
+    next = counter*counter;
+    cout << " " << next; 
+    counter++;
+  }
+
+  counter = first;
+
+  do{
+    cout << endl << counter;
+    next = counter*counter;
+    cout << " " << next; 
+    counter++;
+  }while(counter < last);
+  
+  return 0;
+  
+}
