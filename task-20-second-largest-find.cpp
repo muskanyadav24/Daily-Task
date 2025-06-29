82572df5adf728a96e35835dcd39c76a84890448
Muskan yadav <muskanyadav92163@gmail.com>
2025-06-29 08:24:02 +0530

    Task-20-haf-complete

--- task-20-second-largest-find.cpp
+++ task-20-second-largest-find.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
 
+  int n;
+
+  cout << "Enter size of size :- " ;
+  cin >> n;
+
+  if(n > 2){
+    cout << "Array must have at least 2 element." << endl;
+  }else{
+    int *arr = new int [n];
+    cout << "Enter " << n << " positive integers :- " << endl;
+    for(int i=0;i<n;i++){
+      cin >> arr[i];
+    }
+  }
+  
+  return 0;
+}
