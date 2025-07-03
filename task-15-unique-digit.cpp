bd4c02a4481cf6a8a74a44013e745585ab44c1fc
Muskan yadav <muskanyadav92163@gmail.com>
2025-07-03 14:00:35 +0530

    Task-15-complete

--- task-15-unique-digit.cpp
+++ task-15-unique-digit.cpp
@@ -1,1 +1,33 @@
+#include<iostream>
+using namespace std;
 
+void countUniqueDigits(int num){
+
+  int digits[10] ={0};
+  while (num > 0){
+    int digit = num%10;
+    digits[digit] = 1 ;
+    num = num/10;
+  }
+  int count = 0;
+  cout << "Unique digits :- ";
+  for(int i=0;i<10;i++){
+    if(digits[i] == 1){
+      cout << i << " ";
+      count++;
+    }
+  }
+  cout << endl << "Total unique digits :- " << count << endl;
+}
+
+int main(){
+
+  int num;
+
+  cout << "Enter a number :- ";
+  cin >> num;
+
+  countUniqueDigits(num);
+  
+  return 0;
+}
