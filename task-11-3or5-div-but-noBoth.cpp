cfadbad82fb77bb88e2eaf9571ad9bb2348a6bd1
Muskan yadav <muskanyadav92163@gmail.com>
2025-07-03 13:38:53 +0530

    Task-11-complete

--- task-11-3or5-div-but-noBoth.cpp
+++ task-11-3or5-div-but-noBoth.cpp
@@ -1,1 +1,30 @@
+#include<iostream>
+using namespace std;
 
+void printValidNumbers(int start, int end){
+
+  for(int i=start;i<=end;i++){
+    if((i%3 == 0 || i%5 == 0) && (i%5 != 0)){
+      if((i%3 == 0 || i%5 == 0) && (i%5 !=0)){
+        cout << i << " ";
+      }
+    }
+    cout << endl;
+  }
+  
+}
+
+int main(){
+
+  int start,end;
+
+  cout << "Enter start number :- " ;
+  cin >> start;
+  cout << "Enter end number :- " ;
+  cin >> end;
+
+  cout << "Numbers divisible by 3 or 5 but not both." << endl;
+  printValidNumbers(start,end);
+
+  return 0;
+}
