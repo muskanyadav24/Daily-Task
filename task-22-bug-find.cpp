170af671292137a505f72ff40881c28e347d7709
Muskan yadav <muskanyadav92163@gmail.com>
2025-07-03 14:16:39 +0530

    Task-22-complete

--- task-22-bug-find.cpp
+++ task-22-bug-find.cpp
@@ -1,1 +1,34 @@
+// Fix the bug in given snipet.
 
+#include<iostream>
+using namespace std;
+int main(){
+
+  bool b(int n){
+    for(int i=2;i<=n/2;i++){
+      if(n%i = 0){ // ---> bug (n%i == 0);
+        return false;
+      }
+    }
+    return true;
+  }
+
+  int c(int n){
+    int f=0; // ---> bug (f=1);
+    for(int i=1;i<=n;i++){
+      f*=i;
+    }
+    return f;
+  }
+
+  int a(int n){
+    int rev=0;
+    while(n > 0){
+      rev = rev * 10 + n%10;
+      n/10;  // ---> (n/=10);
+    }
+    return rev;
+  }
+  
+  return 0;
+}
