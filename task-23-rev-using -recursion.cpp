b8f0479f9bed694eb4de45dcd45b88a292b6e9ba
Muskan yadav <muskanyadav92163@gmail.com>
2025-07-05 09:04:34 +0530

    Update task-23-rev-using -recursion.cpp

--- task-23-rev-using -recursion.cpp
+++ task-23-rev-using -recursion.cpp
@@ -1,1 +1,30 @@
+#include<iostream>
+using namespace std;
 
+int reverseNumber(int n, int rev=0){
+  
+  if(n == 0){
+    return rev;
+  }
+  return reverseNumber(n/10, rev*10+n%10);
+}
+
+int main(){
+
+  int num;
+
+  cout << "Enter a number :- ";
+  cin >> num;
+
+  int reversed = reverseNumber(num);
+
+  cout << "Reversed Number :- " << reversed << endl;
+  
+  return 0;
+}
+
+
+// OUTPUT :- 
+
+// Enter a number :- 12345
+// Reversed Number :- 54321
