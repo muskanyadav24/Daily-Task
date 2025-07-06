e412e81e39456d859925915ee0aeda037f0578b0
Muskan yadav <muskanyadav92163@gmail.com>
2025-07-06 08:58:43 +0530

    Task-24-complete

--- task-24-Armstrong-num.cpp
+++ task-24-Armstrong-num.cpp
@@ -1,1 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
 
+  int num,temp,k,sum=0,digits=0;
+
+  cout << "Enter a number :- ";
+  cin >> num;
+
+  temp = num;
+
+  while(temp > 0){
+    digits++;
+    temp /= 10;
+  }
+  temp = num;
+  while(temp > 0){
+    k = temp % 10;
+    int power = 1;
+    for(int i=0;i<digits;i++){
+      power *= k;
+    }
+    sum += power;
+    temp /= 10;
+  }
+  if(sum == originalNum){
+    cout << originalNum << " is an Armstrong number." << endl;
+  }else{
+    cout << originalNum << " is not an Armstrong number." << endl;
+  }
+  
+  return 0;
+}
